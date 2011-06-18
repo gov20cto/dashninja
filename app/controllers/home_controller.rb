af41c5fe64e74965956a1bbf84225449224af0a7
Javier Muniz <javier@granicus.com>
2011-06-17 18:03:46 -0700

    more width changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
     @widgets = [ 
-      { :name => 'burndowns', :update => 30, :width => 1080, :class => 'middle'},
-      { :name => 'grid', :update => 30, :width => 1080 }
+      { :name => 'burndowns', :update => 30, :width => 1000, :class => 'middle'},
+      { :name => 'grid', :update => 30, :width => 1000 }
     ]
   end
 end
